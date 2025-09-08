c04f29dc74392901402b34d4d14659f8f76de544
HanifMahendra <hanifwdyt4@gmail.com>
2025-09-08 19:12:48 +0700

    Inisialisasi full proyek

---
+++ main/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)  # nama item
+    price = models.IntegerField()  # harga item
+    description = models.TextField()  # deskripsi item
+    thumbnail = models.URLField()  # URL gambar item
+    category = models.CharField(max_length=50)  # kategori item
+    is_featured = models.BooleanField(default=False)  # status unggulan
+
+    # opsional tambahan
+    stock = models.IntegerField(default=0)
+    brand = models.CharField(max_length=50, blank=True, null=True)
+    rating = models.FloatField(default=0.0)
+
+    def __str__(self):
+        return self.name
