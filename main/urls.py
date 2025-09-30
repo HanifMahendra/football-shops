873d5bf89a38cc34aff339bb266d92fe2e01cbda
HanifMahendra <hanifwdyt4@gmail.com>
2025-09-30 16:00:12 +0700

    Tugas 5

--- main/urls.py
+++ main/urls.py
@@ -17,4 +17,6 @@ urlpatterns = [
     path('register/', register, name='register'),
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
+    path("product/<int:pk>/edit/", edit_product, name="edit_product"),
+    path("product/<int:pk>/delete/", delete_product, name="delete_product"),
 ]
--- main/views.py
+++ main/views.py
@@ -98,3 +98,21 @@ def logout_user(request):
     response = HttpResponseRedirect(reverse('main:login'))
     response.delete_cookie('last_login')
     return response
+
+def edit_product(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == "POST":
+        form = ProductForm(request.POST, instance=product)
+        if form.is_valid():
+            form.save()
+            return redirect("product_list")
+    else:
+        form = ProductForm(instance=product)
+    return render(request, "edit_product.html", {"form": form})
+
+def delete_product(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == "POST":
+        product.delete()
+        return redirect("product_list")
+    return render(request, "delete_confirm.html", {"product": product})
